b71d03b8746edd28ea03b1a69e7ecb1e1c52d860
Rusira <rusirarh25@gmail.com>
2021-04-28 13:40:22 +0530

    Initial Commit

---
+++ Nibmcwfinal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nibmcwfinal
+//
+//  Created by Rusira on 4/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

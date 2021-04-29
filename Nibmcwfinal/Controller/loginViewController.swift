0c87ecf9a350b06f7e6b723c651b7757dce2a466
Rusira <rusirarh25@gmail.com>
2021-04-29 16:57:06 +0530

    Fix Errors

--- Nibmcwfinal/Controller/loginViewController.swift
+++ Nibmcwfinal/Controller/loginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class loginViewController: UIViewController {
     
@@ -13,8 +14,7 @@ class loginViewController: UIViewController {
     @IBOutlet weak var txtemail: UITextField!
     @IBOutlet weak var txtpassword: UITextField!
     
-    var number: Int?
-    var fetchedNumber: Int = 0
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,13 +22,57 @@ class loginViewController: UIViewController {
         
     }
     @IBAction func onloginpressed(_ sender: UIButton) {
-    
-        if let email = txtemail.text {
-            if email.count < 5 {
-                
-            }
+        if validateInput() {
+            authenticateuser(email: txtemail.text!, password: txtpassword.text!)
+            
+            
+        } else {
+            print("Input Error Found")
             
         }
+        
+        
     }
     
-}
+    func validateInput() -> Bool {
+        
+
+        guard let email = txtemail.text else {
+            print("Email is Null")
+            return false
+        }
+        
+        guard let password = txtpassword.text else {
+            print("Password is Null")
+            return false
+        }
+        
+        if email.count < 5 {
+            print("Enter valid Email")
+            return false
+        }
+        
+        if password.count < 5 {
+            print("Enter valid password")
+            return false        }
+        
+        return true
+    }
+    
+    func authenticateuser(email: String, password: String) {
+        Auth.auth().signIn(withEmail: email, password: password) {
+            authResult, error in
+            
+            if let err = error {
+                print(err.localizedDescription)
+                return
+            }
+            
+            if let result = authResult {
+                print("user Email: \(result.user.email ?? "No Found")")
+            }
+                
+            }
+        }
+        
+    }

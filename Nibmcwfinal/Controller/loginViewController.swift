a05e2307d4a3897c8a32b59e374647d1886fac80
Rusira <rusirarh25@gmail.com>
2021-04-29 12:30:06 +0530

    Validate Login TextFields

--- Nibmcwfinal/Controller/loginViewController.swift
+++ Nibmcwfinal/Controller/loginViewController.swift
@@ -8,22 +8,27 @@
 import UIKit
 
 class loginViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var txtemail: UITextField!
+    @IBOutlet weak var txtpassword: UITextField!
+    
+    var number: Int?
+    var fetchedNumber: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
+    }
+    @IBAction func onloginpressed(_ sender: UIButton) {
+    
+        if let email = txtemail.text {
+            if email.count < 5 {
+                
+            }
+            
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
---
+++ Nibmcwfinal/Model/entitymodel.swift
@@ -0,0 +1,32 @@
+//
+//  entitymodel.swift
+//  Nibmcwfinal
+//
+//  Created by Rusira on 4/29/21.
+//
+
+import Foundation
+
+struct user {
+    var username: String
+    var useremail: String
+    var userpassword: String
+    var userphone: String
+    
+}
+
+struct fooditem {
+    var _id: String
+    var foodname: String
+    var foodprice: Double
+    var discount: Int
+}
+
+
+//class user1 {
+    
+       // self.username = username
+       // self.useremail = useremail
+      //  self.userpassword = userpassword
+    //    self.userphone = userphone
+  //  }
